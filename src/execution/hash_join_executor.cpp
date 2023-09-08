221031f59f393ded088d9a4bb6a0d4df2d170853
Tsing <874479171@qq.com>
2023-09-08 09:58:11 +0800

    project3 hashjoin有问题

--- src/execution/hash_join_executor.cpp
+++ src/execution/hash_join_executor.cpp
@@ -11,21 +11,84 @@
 //===----------------------------------------------------------------------===//
 
 #include "execution/executors/hash_join_executor.h"
+#include "type/value.h"
 
 namespace bustub {
 
 HashJoinExecutor::HashJoinExecutor(ExecutorContext *exec_ctx, const HashJoinPlanNode *plan,
                                    std::unique_ptr<AbstractExecutor> &&left_child,
                                    std::unique_ptr<AbstractExecutor> &&right_child)
-    : AbstractExecutor(exec_ctx) {
+    : AbstractExecutor(exec_ctx),
+      plan_{plan},
+      left_executor_{std::move(left_child)},
+      right_executor_(std::move(right_child)) {
   if (!(plan->GetJoinType() == JoinType::LEFT || plan->GetJoinType() == JoinType::INNER)) {
     // Note for 2023 Spring: You ONLY need to implement left join and inner join.
     throw bustub::NotImplementedException(fmt::format("join type {} not supported", plan->GetJoinType()));
   }
 }
 
-void HashJoinExecutor::Init() { throw NotImplementedException("HashJoinExecutor is not implemented"); }
+void HashJoinExecutor::Init() {
+  left_executor_->Init();
+  right_executor_->Init();
 
-auto HashJoinExecutor::Next(Tuple *tuple, RID *rid) -> bool { return false; }
+  Tuple tmp_tuple{};
+  RID rid;
+  while (right_executor_->Next(&tmp_tuple, &rid)) {
+    for (auto &e : plan_->RightJoinKeyExpressions()) {
+      auto join_key = e->Evaluate(&tmp_tuple, plan_->GetRightPlan()->OutputSchema());
+      hash_join_table_[HashUtil::HashValue(&join_key)].push_back(tmp_tuple);
+    }
+  }
+
+  while (left_executor_->Next(&tmp_tuple, &rid)) {
+    for (auto &e : plan_->LeftJoinKeyExpressions()) {
+      auto join_key = e->Evaluate(&tmp_tuple, plan_->GetLeftPlan()->OutputSchema());
+      if (hash_join_table_.count(HashUtil::HashValue(&join_key)) > 0) {
+        auto right_tuples = hash_join_table_[HashUtil::HashValue(&join_key)];
+        for (const auto &tuple : right_tuples) {
+          for (auto &e2 : plan_->RightJoinKeyExpressions()) {
+            auto right_join_key = e2->Evaluate(&tuple, plan_->GetRightPlan()->OutputSchema());
+            if (right_join_key.CompareEquals(join_key) == CmpBool::CmpTrue) {
+              std::vector<Value> values{};
+              values.reserve(plan_->GetLeftPlan()->OutputSchema().GetColumnCount() +
+                             plan_->GetRightPlan()->OutputSchema().GetColumnCount());
+              for (uint32_t col_idx = 0; col_idx < plan_->GetLeftPlan()->OutputSchema().GetColumnCount(); col_idx++) {
+                values.push_back(tmp_tuple.GetValue(&plan_->GetLeftPlan()->OutputSchema(), col_idx));
+              }
+              for (uint32_t col_idx = 0; col_idx < plan_->GetRightPlan()->OutputSchema().GetColumnCount(); col_idx++) {
+                values.push_back(tuple.GetValue(&plan_->GetRightPlan()->OutputSchema(), col_idx));
+              }
+              output_tuples_.emplace_back(values, &GetOutputSchema());
+            }
+          }
+        }
+      } else if (plan_->GetJoinType() == JoinType::LEFT) {
+        std::vector<Value> values{};
+        values.reserve(plan_->GetLeftPlan()->OutputSchema().GetColumnCount() +
+                       plan_->GetRightPlan()->OutputSchema().GetColumnCount());
+        for (uint32_t col_idx = 0; col_idx < plan_->GetLeftPlan()->OutputSchema().GetColumnCount(); col_idx++) {
+          values.push_back(tmp_tuple.GetValue(&plan_->GetLeftPlan()->OutputSchema(), col_idx));
+        }
+        for (uint32_t col_idx = 0; col_idx < plan_->GetRightPlan()->OutputSchema().GetColumnCount(); col_idx++) {
+          values.push_back(
+              ValueFactory::GetNullValueByType(plan_->GetRightPlan()->OutputSchema().GetColumn(col_idx).GetType()));
+        }
+        output_tuples_.emplace_back(values, &GetOutputSchema());
+      }
+    }
+  }
+
+  output_tuples_iter_ = output_tuples_.cbegin();
+}
+
+auto HashJoinExecutor::Next(Tuple *tuple, RID *rid) -> bool {
+  if (output_tuples_iter_ == output_tuples_.cend()) {
+    return false;
+  }
+  *tuple = *output_tuples_iter_;
+  ++output_tuples_iter_;
+  return true;
+}
 
 }  // namespace bustub
--- src/optimizer/nlj_as_hash_join.cpp
+++ src/optimizer/nlj_as_hash_join.cpp
@@ -1,5 +1,7 @@
 #include <algorithm>
 #include <memory>
+#include <utility>
+#include <vector>
 #include "catalog/column.h"
 #include "catalog/schema.h"
 #include "common/exception.h"
@@ -7,11 +9,13 @@
 #include "execution/expressions/column_value_expression.h"
 #include "execution/expressions/comparison_expression.h"
 #include "execution/expressions/constant_value_expression.h"
+#include "execution/expressions/logic_expression.h"
 #include "execution/plans/abstract_plan.h"
 #include "execution/plans/filter_plan.h"
 #include "execution/plans/hash_join_plan.h"
 #include "execution/plans/nested_loop_join_plan.h"
 #include "execution/plans/projection_plan.h"
+#include "fmt/core.h"
 #include "optimizer/optimizer.h"
 #include "type/type_id.h"
 
@@ -22,7 +26,63 @@ auto Optimizer::OptimizeNLJAsHashJoin(const AbstractPlanNodeRef &plan) -> Abstra
   // Note for 2023 Spring: You should at least support join keys of the form:
   // 1. <column expr> = <column expr>
   // 2. <column expr> = <column expr> AND <column expr> = <column expr>
-  return plan;
+  std::vector<AbstractPlanNodeRef> children;
+  for (const auto &child : plan->GetChildren()) {
+    children.emplace_back(OptimizeNLJAsHashJoin(child));
+  }
+  auto optimized_plan = plan->CloneWithChildren(std::move(children));
+
+  if (optimized_plan->GetType() == PlanType::NestedLoopJoin) {
+    const auto &nlj_plan = dynamic_cast<const NestedLoopJoinPlanNode &>(*optimized_plan);
+    // Has exactly two children
+    BUSTUB_ENSURE(nlj_plan.children_.size() == 2, "NLJ should have exactly 2 children.");
+
+    // Check if expr is equal condition where one is for the left table, and one is for the right table.
+    if (const auto *expr = dynamic_cast<const ComparisonExpression *>(nlj_plan.Predicate().get()); expr != nullptr) {
+      // t1.a = t2.a
+      if (expr->comp_type_ == ComparisonType::Equal) {
+        if (const auto *left_expr = dynamic_cast<const ColumnValueExpression *>(expr->children_[0].get());
+            left_expr != nullptr) {
+          std::vector<AbstractExpressionRef> left_exprs;
+          std::vector<AbstractExpressionRef> right_exprs;
+          for (auto const &expr : expr->children_) {
+            if (dynamic_cast<ColumnValueExpression *>(expr.get())->GetTupleIdx() == 0) {
+              left_exprs.emplace_back(expr);
+            } else {
+              right_exprs.emplace_back(expr);
+            }
+          }
+          return std::make_shared<HashJoinPlanNode>(nlj_plan.output_schema_, nlj_plan.GetLeftPlan(),
+                                                    nlj_plan.GetRightPlan(), std::move(left_exprs),
+                                                    std::move(right_exprs), nlj_plan.GetJoinType());
+        }
+      }
+    }
+    if (const auto *expr = dynamic_cast<const LogicExpression *>(nlj_plan.Predicate().get()); expr != nullptr) {
+      // t1.a = t2.a and t1.b = t2.b
+      if (expr->logic_type_ == LogicType::And) {
+        std::vector<AbstractExpressionRef> left_exprs;
+        std::vector<AbstractExpressionRef> right_exprs;
+        for (const auto &left_expr : expr->children_) {
+          for (auto const &expr : left_expr->children_) {
+            if (dynamic_cast<ColumnValueExpression *>(expr.get())->GetTupleIdx() == 0) {
+              left_exprs.emplace_back(expr);
+            } else {
+              right_exprs.emplace_back(expr);
+            }
+          }
+        }
+        auto plan_temp =
+            std::make_shared<HashJoinPlanNode>(nlj_plan.output_schema_, nlj_plan.GetLeftPlan(), nlj_plan.GetRightPlan(),
+                                               std::move(left_exprs), std::move(right_exprs), nlj_plan.GetJoinType());
+        fmt::print("{}\n", plan_temp->ToString());
+        // fmt::print("{}\n", plan_vec.size());
+        return plan_temp;
+      }
+    }
+  }
+
+  return optimized_plan;
 }
 
 }  // namespace bustub
